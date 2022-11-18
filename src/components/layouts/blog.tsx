7aa1e687488f64be6d97125d8b639fb2b0c6dafa
Frans Filasta Pratama <fransfilastap@live.com>
2022-11-18 22:05:55 +0700

    refactor(UI): change styling

--- src/components/layouts/blog.tsx
+++ src/components/layouts/blog.tsx
@@ -1,5 +1,16 @@
 import { PropsWithChildren, Suspense } from 'react'
-import { Box, SkeletonText, chakra, Flex, Heading, HStack, Text, useColorModeValue, Container } from '@chakra-ui/react'
+import {
+  Box,
+  SkeletonText,
+  chakra,
+  Flex,
+  Heading,
+  HStack,
+  Text,
+  useColorModeValue,
+  Container,
+  VStack
+} from '@chakra-ui/react'
 import config from '@/lib/config'
 import { format, parseISO } from 'date-fns'
 import { Prose } from '@nikolovlazar/chakra-ui-prose'
@@ -10,7 +21,7 @@ import { ChakraNextImage } from '@/components/ChakraNextImage'
 import { Post } from '@/lib/types'
 import ViewCounter from '@/components/ViewCounter'
 import { FiClock } from 'react-icons/fi'
-import PostAttribute from '../PostAttribute'
+import PostAttribute from '@/components/PostAttribute'
 
 export default function BlogLayout({ children, post }: PropsWithChildren<{ post: Post }>) {
   const blogTitle = `${config.site_url}/blog/${post.title}`
@@ -30,20 +41,21 @@ export default function BlogLayout({ children, post }: PropsWithChildren<{ post:
               {post.title}
             </Heading>
             <Flex flexDir={{ base: 'column', md: 'row' }} justifyContent='space-between' mb={{ base: 6, md: 4 }}>
-              <HStack>
-                <ChakraNextImage
-                  borderRadius='3xl'
-                  imageFit='cover'
-                  imageFitPosition='center'
-                  width={25}
-                  height={25}
-                  alt='Author Photo'
-                  src='https://avatars.githubusercontent.com/u/10008396?v=4'
-                />
-                <Text fontSize={{ base: 'sm', md: 'sm' }} color={useColorModeValue('gray.700', 'gray.500')}>
-                  Frans Filasta Pratama / {format(parseISO(post.date), 'MMMM dd, yyyy')}
-                </Text>
-              </HStack>
+              <VStack justifyContent={'flex-start'} alignItems={'start'} gap={0}>
+                <HStack>
+                  <ChakraNextImage
+                    borderRadius='3xl'
+                    width={25}
+                    height={25}
+                    alt={'Author Photo'}
+                    src='https://avatars.githubusercontent.com/u/10008396?v=4'
+                  />
+                  <Text fontSize={{ base: 'sm', md: 'sm' }} color={useColorModeValue('gray.700', 'gray.500')}>
+                    <span>Frans Filasta Pratama</span>
+                    <span> / {format(parseISO(post.date), 'MMMM dd, yyyy')}</span>
+                  </Text>
+                </HStack>
+              </VStack>
               <HStack>
                 <ViewCounter slug={post.slug} />-<PostAttribute icon={FiClock}>{post.readingTime}</PostAttribute>
               </HStack>
@@ -53,7 +65,7 @@ export default function BlogLayout({ children, post }: PropsWithChildren<{ post:
                 width='full'
                 height={{ base: '30vh', md: '60vh' }}
                 borderRadius='xl'
-                src={post.images[0]}
+                src={post.cover}
                 alt={post.title}
               />
             )}
