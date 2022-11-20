67d93ea3067c72ee73e20e2556db0f9d11cb4142
Frans Filasta Pratama <fransfilastap@live.com>
2022-11-21 06:33:24 +0700

    refactor(UI): remove video, dark mode font color

--- src/components/PostAttribute.tsx
+++ src/components/PostAttribute.tsx
@@ -3,12 +3,12 @@ import React, { PropsWithChildren } from 'react'
 import { IconType } from 'react-icons/lib'
 
 export type PostAttributeProps = {
-  icon: IconType
+  icon?: IconType
 } & ChakraProps
 
 const PostAttribute: React.FunctionComponent<PropsWithChildren<PostAttributeProps>> = (props): React.ReactElement => (
   <HStack justifyContent='flex-start' gap={0}>
-    <Icon as={props.icon} color={props.color} size='sm' />
+    {props.icon && <Icon as={props.icon} color={props.color} size='sm' />}
     <chakra.span {...props} fontSize={{ base: 'sm', md: 'sm' }}>
       {props.children}
     </chakra.span>
