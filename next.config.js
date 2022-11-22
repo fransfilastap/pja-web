84b6cd21e4d2227ea5e146dda27458a9f48a063f
Frans Filasta Pratama <fransfilastap@live.com>
2022-11-22 16:02:10 +0700

    chore: add @next/fonts

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['avatars.githubusercontent.com', 'how-to-bphn.notion.site', 'images.unsplash.com']
+    domains: ['avatars.githubusercontent.com', 'images.unsplash.com']
   },
   experimental: {
     forceSwcTransforms: false
