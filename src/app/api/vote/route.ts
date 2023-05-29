825e50bf83a3220e970e34733f5c7896c51fe804
fransfilastap@live.com <fransfilastap@Live.com>
2023-05-29 22:39:00 +0700

    fix: api level checking

--- src/app/api/vote/route.ts
+++ src/app/api/vote/route.ts
@@ -4,7 +4,35 @@ import { NextRequest, NextResponse } from "next/server"
 import { authOptions } from "@/lib/auth";
 
 export async function POST(req: NextRequest) {
+
+    const isVotingDone = await prisma.appConfig.findFirst({
+        where: {
+            key: "IS_VOTING_DONE",
+        },
+    });
+
+    const isVotingStart = await prisma.appConfig.findFirst({
+        where: {
+        key: "IS_VOTING_START",
+        },
+    });    
+
+    if (!isVotingStart?.value) {
+        return NextResponse.json({
+            status: "voting not started",
+        }, {
+            status: 401
+        })
+    }
     
+    if (isVotingDone?.value) {
+        return NextResponse.json({
+            status: "voting closed",
+        }, {
+            status: 401
+        })
+    }
+
     const session = await getServerSession(authOptions)
     if (session) {
         const body = await req.json();     
@@ -23,9 +51,9 @@ export async function POST(req: NextRequest) {
         return NextResponse.json({
             status: "success",
         })
-    } else {
-        return NextResponse.json({ status:'error',message:'unauthorized'},{status:401})
     }
+
+    return NextResponse.json({ status:'error',message:'unauthorized'},{status:401})
 }
 
    
