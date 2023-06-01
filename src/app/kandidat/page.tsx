e4b62e6879090b3f2021fb78a6cef106ea2238f1
fransfilastap@live.com <fransfilastap@Live.com>
2023-06-01 11:28:03 +0700

    fix: page heading

--- src/app/kandidat/page.tsx
+++ src/app/kandidat/page.tsx
@@ -49,7 +49,9 @@ export default async function Page() {
     <Container className="p-6 my-20">
       <div className="flex flex-col items-start justify-between mb-10">
         <h2 className="font-[600] py-5 text-4xl font-heading text-transparent bg-clip-text bg-gradient-to-br from-[#FCAA43] from-5% via-[#943C30] via-20% to-[#5941A9] to-75%">
-          Nominee
+          {votingDone
+            ? "Top 10 Favorit Publik Paralegal Justice Award"
+            : "Nominee"}
         </h2>
       </div>
 
